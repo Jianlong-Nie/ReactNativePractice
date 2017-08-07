b9aa3009038be1be1db500db97fa66a8004e79cb
JianLong <1317272927@qq.com>
2017-08-07 16:33:47 +0800

    初始化

---
+++ index.ios.js
@@ -0,0 +1,84 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+import { Button, TabBar, Icon } from 'antd-mobile';
+import Home from './js/Home';
+export default class myapp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedTab: 'redTab',
+            hidden: false,
+        };
+    }
+    renderContent(pageText) {
+        return <Text></Text>;
+    }
+    render() {
+        return (
+            <TabBar
+                unselectedTintColor="#949494"
+                tintColor="#33A3F4"
+                barTintColor="white"
+                hidden={this.state.hidden}
+            >
+                <TabBar.Item
+                    title="生活"
+                    key="生活"
+                    selected={this.state.selectedTab === 'blueTab'}
+                    onPress={() => {
+                        this.setState({
+                            selectedTab: 'blueTab',
+                        });
+                    }}
+                    data-seed="logId"
+                >
+                    {Home}
+                </TabBar.Item>
+                <TabBar.Item
+                    title="我的"
+                    key="我的"
+                    selected={this.state.selectedTab === 'yellowTab'}
+                    onPress={() => {
+                        this.setState({
+                            selectedTab: 'yellowTab',
+                        });
+                    }}
+                >
+                    {this.renderContent('我的')}
+                </TabBar.Item>
+            </TabBar>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
+
+AppRegistry.registerComponent('myapp', () => myapp);
